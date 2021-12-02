98c0a9be8c5b3504fd7a81dac833300ff24fda95
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-12-02 19:48:01 +0100

    Setup the app

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import '../Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>App</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
