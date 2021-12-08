9d981052fe00226f9a0f863869f0dbc949dba091
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-12-08 20:56:01 +0100

    Update Navbar and create helper tools

---
+++ src/Library/links.js
@@ -0,0 +1,28 @@
+const links = [
+  {
+    text: 'Home',
+    path: 'home',
+  },
+  {
+    text: 'Categories',
+    path: 'categories',
+  },
+  {
+    text: 'Leaderboard',
+    path: 'leaderboard',
+  },
+  {
+    text: 'Profile',
+    path: 'profile',
+  },
+  {
+    text: 'Play',
+    path: 'play',
+  },
+  {
+    text: 'Login',
+    path: 'login',
+  },
+];
+
+export default links;
