8488a08e265cead603b146d1da2c818be030bd2f
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-12-14 20:23:29 +0100

    Created Profile component

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import mockUser from '../Library/mockUser';
+import '../Styles/Profile.css';
 
-const Profile = () => (
-  <div />
-);
+const Profile = () => {
+  let url;
+  if (mockUser.avatar) url = mockUser.avatar;
+  else url = 'programmer.png';
+  return (
+    <>
+      <header className="profile-header">
+        <img src={url} alt="avatar" />
+        <div className="info">
+          <h3>
+            Name:
+            <span>{` ${mockUser.name}`}</span>
+          </h3>
+          <h3>
+            E-mail:
+            <span>{` ${mockUser.email}`}</span>
+          </h3>
+        </div>
+      </header>
+      <div className="best-scores">
+        <h3>Best scores</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Categories</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mockUser.bestScores.map((category) => (
+              <tr key={uuidv4()}>
+                <td>{Object.keys(category)[0]}</td>
+                <td>{category[Object.keys(category)[0]]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="trophies">
+        <h3>Trophies</h3>
+        {mockUser.trophies.map((trophy) => (
+          <h4 key={uuidv4()}>{trophy}</h4>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Profile;
---
+++ src/Library/mockUser.js
@@ -0,0 +1,19 @@
+const mockUser = {
+  name: 'User1',
+  email: 'user@mail.com',
+  avatar: 'programmer.png',
+  bestScores: [
+    {
+      HTML: 124322,
+    },
+    {
+      CSS: 124452,
+    },
+    {
+      JavaScript: 1243362,
+    },
+  ],
+  trophies: ['bone-marrow', 'make-up artist', 'conditional-Trainer'],
+};
+
+export default mockUser;
