db009199d75a91114d391d6fdda9e0429de72dbd
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2022-01-02 20:25:09 +0100

    Created countdown watch

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Countdown from 'react-countdown';
 import GameMenu from './GameMenu';
 import GameQuestion from './GameQuestion';
 import '../Styles/Game.css';
 import GameAnswer from './GameAnswer';
 
+const renderer = ({ minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a complete state
+    return <span>Time is up! Go on next question!</span>;
+  }
+  // Render a countdown
+  return (
+    <span>
+      {minutes}
+      :
+      {seconds}
+    </span>
+  );
+};
 const Game = () => (
   <div className="game">
     <header>
@@ -15,6 +30,9 @@ const Game = () => (
       <div className="left-side">
         <GameAnswer answer="Yes" />
         <GameAnswer answer="No" />
+      </div>
+      <div className="timer">
+        <Countdown date={Date.now() + 90000} renderer={renderer} />
       </div>
       <div className="right-side">
         <GameAnswer answer="Maybe" />
